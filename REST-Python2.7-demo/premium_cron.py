23355d0dae836371ecd16d098a45d0978722bcc2
jhonye <jhonyelink@gmail.com>
2019-07-08 22:41:54 +0800

    ok1

--- REST-Python2.7-demo/premium_cron.py
+++ REST-Python2.7-demo/premium_cron.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     import os
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    monitor_period = 5
+    monitor_period = 3
     send_flag = False
     with open('config/alarm.yml', 'r') as stream:
         monitor = yaml.load(stream, Loader=Loader)
@@ -119,7 +119,8 @@ if __name__ == '__main__':
         '''
         table_data.append(table_line)
 
-    bodong_list = []
+    up_list = []
+    down_list = []
     send_data = ""
     send_data += "## " + ", ".join(table_title) + '\n'
     for line in table_data:
@@ -131,14 +132,17 @@ if __name__ == '__main__':
         if float(line[1]) > threshold + monitor_period \
                 or float(line[1]) < threshold - monitor_period:
             send_flag = True
-            bodong_list.append(line[0])
             if line[0] not in monitor['MONITOR']:
                 monitor['MONITOR'][line[0]] = {}
 
             next_threshold = float(line[1]) - float(line[1]) % monitor_period
-            monitor['MONITOR'][line[0]]['threshold'] = next_threshold \
-                if float(line[1]) > threshold - monitor_period \
-                else next_threshold + monitor_period
+            if float(line[1]) < threshold + monitor_period:
+                next_threshold += monitor_period
+                down_list.append(line[0])
+            else:
+                up_list.append(line[0])
+
+            monitor['MONITOR'][line[0]]['threshold'] = next_threshold
 
     from datetime import datetime
     import pytz
@@ -146,7 +150,8 @@ if __name__ == '__main__':
 
     send_data += "\n\n### " + cn_dt.strftime('%Y-%m-%d %H:%M:%S')
     send_data += "\n### 消息发送条件为上下波动%d个点" % monitor_period
-    send_data += "\n### %s " % ', '.join(bodong_list)
+    send_data += "\n### 扩大: %s " % ', '.join(up_list)
+    send_data += "\n### 缩小: %s " % ', '.join(down_list)
 
     DING_DING_MARKDOWN_TEMPLATE['markdown']['text'] = send_data
     if send_flag:
